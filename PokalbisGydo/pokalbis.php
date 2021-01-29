fe6ace2203484bb060946475c52c8777a87dd060
Dainius Karpavicius <60507404+dainaklis@users.noreply.github.com>
2021-01-29 15:44:45 +0200

    Add files via upload

---
+++ PokalbisGydo/pokalbis.php
@@ -0,0 +1,47 @@
+<?php include "header.php" ?>
+
+
+<div class="pokalbis">
+    <div class="apieMane_inside">
+        <h1 class="juodah1">Pokalbis</h1>
+        <img src="images/puodelis 2n.jpeg" />
+        <p class="juodap"><strong>Kas yra pokalbis?</strong> Pokalbis sutelkia dėmesį į asmens potencialo plėtrą, o ne į asmenybės problemas, atsirandančias iš vaikystės, t. y. yra kalbama apie dabarties jausmus, o ne apie vaikystėje patirtas emocijas. Pokalbis geriausiai veikia, kai žmogus gali atvirai papasakoti apie savo jausmus, kurie gali būti nepatogūs, skausmingi ar labai asmeniški. Mes intuityviai stengiamės apsaugoti žmones, kuriuos mylime, todėl negalime atvirai pasidalinti savo tikrosiomis mintimis ir jaučiamais jausmais. Daugelis žmonių būna atviresni su kitu nešališku asmeniu, kuris yra linkęs klausytis ir padėti asmeniui suvokti save bei rasti atsakymą į rūpimą klausimą. </p>
+        <p class="juodap"><strong>Kaip vyksta, kiek trunka?</strong> Susitikimas vyksta gyvai ir trunka 45 minutes. Dažniausiai užtenka 5-10 susitikimų. Susitikimo metu kalbame apie viską, ką Jūs norite aptarti, kokiomis mintimis norite pasidalinti, bandome surasti atsakymus į klausimus, kurie Jums neduoda ramybės. </p>
+        <p class="juodap"><strong>Mano darbo filosofija.</strong> Pagrindiniai moralės stulpai, kuriais paremtas mano bendravimas, yra:</p>
+        <p class="juodapnum">&nbsp;-Tolerancija. Kiekvieno žmogaus skirtumų priėmimas ir supratimas be nusistatymo bei su noru pažinti.</p>
+        <p class="juodapnum">&nbsp;-Pagarba. Kiekvienas žodis ir jį lydintis kūno judesys bei emocija turi rodyti, kad žmogus yra gerbiamas, suprantamas ir palaikomas.</p>
+        <p class="juodapnum">&nbsp;-Empatija. Savęs įsivaizdavimas kito žmogaus vietoje sukuria galimybę atsiriboti nuo pirminių jausmų išgirdus apie žmogaus elgesį ir jausmus.</p>
+
+        <p class="juodap"><strong>Mano darbo stilius.</strong> Bendraudama su žmonėmis mėgstu improvizuoti. Tačiau svarbiausia, ko siekiu – kad kiekvienas pašnekovas jaustų šiltą bendravimą ir būtų suprastas, o ne išklausytų pamokslą. Todėl susitikimuose taikau įvairias kognityvinės elgesio terapijos* technikas, kurios skirtos konkrečiam pašnekovui ir konkrečiam klausimui spręsti. Susitikimų skaičius priklauso nuo kiekvieno žmogaus situacijos, todėl Jūsų poreikiai ir pageidavimai yra pirmoje vietoje.</p>
+        <p class="juodap"><strong>Kuo tikiu?</strong> Aš tikiu, kad tik kalbėdamas žmogus gali padėti sau. Ir pats tinkamiausias laikas pradėti – dabar. Mūsų galvose sukasi tiek daug minčių apie kasdienius rūpesčius, planus, kitų žmonių elgesį – spėliojame, kodėl vienas ar kitas elgiasi taip ir ne kitaip, svarstome, kodėl kiti reaguoja į mus ne taip, kaip mes to tikimės. Visos mintys sukuria jausmų ir emocijų verpetą, kuris gali mus įtraukti į situacijas, kai norime pasislėpti nuo mus supančio pasaulio arba, atvirkščiai, pradedame maištauti. </p>
+        <p class="juodap">Jeigu tik žmogus papasakotų kitam apie savo tikruosius jausmus, nebijodamas būti nesuprastas ar teisiamas, jis galėtų išvengti klaidingų veiksmų ar netinkamai priimtų sprendimų. Pokalbis – nesudėtingas įrankis, kuris padeda užtikrinti kiekvieno iš mūsų vidinę pusiausvyrą.</p>
+        <p class="juodap"><strong>Ką vertinu?</strong> Mūsų bendravimo pagrindas – atvirumas, noras girdėti save ir imtis veiksmų. </p>
+        <p class="juodap"><strong>Už ką vertina mane?</strong> Dažnai girdimas paaiškinimas – pozityvumas. Save laikau pozityvia realiste. Gyvenimas siuntė ir siunčia daug įvairių situacijų, į kurių sprendimą žiūriu per realių veiksmų ir emocijų valdymo prizmę. Todėl situacijų valdymas be didelio streso sukuria iliuziją, kad esu nepataisoma optimistė.</p>
+        <p class="juodap"><strong>Su kokiais klientai dirbu?</strong> Ieškančiais atsakymo, sprendimo ir jaučiančiais poreikį judėti į priekį, įgarsinant savo mintis, norus ir tikrąsias emocijas. Smagu dirbti su žmonėmis, kurie su laiku supranta, kad tik jie patys gali pasiekti trokštamą savijautą arba inicijuoti pokyčius savo gyvenime. </p>
+        <p class="juodap"><strong>Kokios mano gvildenamos temos? </strong></p>
+        <p class="juodapnum ">- Savęs pažinimas (nuo kasdienių jausmų iki savo pašaukimo atradimo)</p>
+        <p class="juodapnum ">- Vienišumo jausmo supratimas ir jo pažinimas</p>
+        <p class="juodapnum ">- Kopriklausomybės (priklausomos meilės) identifikavimas ir jos valdymas</p>
+        <p class="juodapnum ">- Santykių kūrimas ir palaikymas</p>
+        <p class="juodapnum ">- Atsakomybės jausmo ir / ar perfekcionizmo suvokimas bei priėmimas</p>
+        <p class="juodapnum ">- Savęs adaptavimas visuomenėje</p>
+        <p class="juodapnum ">- Vienišumo jausmo supratimas ir jo pažinimas </p>
+        <p class="juodapnum ">- Artimųjų ligų priėmimas</p>
+        <p class="juodapnum ">- Netekčių priėmimas ir gyvenimas toliau</p>
+        <p class="juodapnum ">- Skyrybos</p>
+        <p class="juodapnum ">- Nevaisingumas</p>
+        
+        <p class="juodap"><strong>Kokią turiu patirtį?</strong> Dešimties metų patirtis analizuojant kognityvinės elgesio terapijos* metodus kursuose, seminaruose ir paskaitose, bei terapijos metodus taikant asmeniniame gyvenime. Penkeri metai konsultavimo asmenų, kuriems buvo reikalingas pokalbis. </p>
+
+        <button class="myFunction" onclick="myFunction()">Užsirašyti susitikimui</button>
+
+        <p class="juodapais">* Kognityvinė elgesio terapija, KET – viena iš psichoterapijos rūšių, aiškinanti, kaip yra susiję žmogaus mintys, emocijos ir elgesys, kokią įtaką žmogaus sprendimams daro jo kertiniai įsitikinimai ir patirtis juos realizuojant. Ši terapija sparčiai populiarėja, nes greitas gyvenimo tempas ir augantys poreikiai skatina žmones ieškoti greito ir į tikslą orientuoto problemų sprendimo būdo.</p>
+
+    </div>
+
+
+</div>
+
+
+
+<?php include "footer.php" ?>
---
+++ PokalbisGydo/scripts/script.js
@@ -0,0 +1,8 @@
+
+    function myFunction() {
+      window.open("registracija.php", "", "width=780, height=750");
+      window.close();
+    }
+
+
+
